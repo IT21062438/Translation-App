0a800f03436941dd14709b9ef9585bb18f93ca49
Harendra Samarawickrama <87431873+IT21062438@users.noreply.github.com>
2025-01-29 00:39:05 +0530

    Created functions

--- frontend/src/components/Translator.js
+++ frontend/src/components/Translator.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import languages from "./languages";
 import { FaVolumeUp, FaCopy, FaExchangeAlt } from "react-icons/fa";
 
 function Translator() {
+  const [fromText, setFromText] = useState("");
+  const [toText, setToText] = useState("");
+  const [fromLanguage, setFromLanguage] = useState("en");
+  const [toLanguage, setToLanguage] = useState("fr");
+
+  const handleExchange = () => {
+    let tempValue = fromText;
+    setFromText(toText);
+    setToText(tempValue);
+
+    let tempLanguage = fromLanguage;
+    setFromLanguage(toLanguage);
+    setToLanguage(tempLanguage);
+  };
+
+  const copyContent = (text) => {
+    navigator.clipboard.writeText(text);
+  };
+  const utterText = (text, language) => {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = language;
+    synth.speak(utterance);
+  };
+
+  const handleIconClick = (action, text, language) => {
+    if (action === "copy") {
+      copyContent(text);
+    } else if (action === "speak") {
+      utterText(text, language);
+    }
+  };
+
   return (
     <div>
       <div className="wrapper">
@@ -11,33 +45,70 @@ function Translator() {
             name="from"
             id="from"
             placeholder="Enter Text"
+            value={fromText}
+            onChange={(e) => setFromText(e.target.value)}
           ></textarea>
-          <textarea className="to-text" name="to" id="to" readOnly></textarea>
+          <textarea
+            className="to-text"
+            name="to"
+            id="to"
+            readOnly
+            value={toText}
+          ></textarea>
         </div>
         <ul className="controls">
           <li className="row from">
             <div className="icons">
-              <FaVolumeUp className="i-from"id="from" />
-              <FaCopy className="i-from" id="from" />
+              <FaVolumeUp
+                className="from-vol"
+                id="from"
+                onClick={() => handleIconClick("speak", fromText, fromLanguage)}
+              />
+              <FaCopy
+                className="from-copy"
+                id="from"
+                onClick={() => handleIconClick("copy", fromText)}
+              />
             </div>
-            <select>
-              <option value={""}>First Option</option>
-              <option value={""}>Second Option</option>
+
+            <select
+              value={fromLanguage}
+              onChange={(e) => setFromLanguage(e.target.value)}
+            >
+              {languages.map(({ code, name }) => (
+                <option key={code} value={code}>
+                  {name}
+                </option>
+              ))}
             </select>
           </li>
-          <li className="change">
+          <li className="change" onClick={handleExchange}>
             <div className="icons">
               <FaExchangeAlt className="i-change" />
             </div>
           </li>
           <li className="row to">
-            <select>
-              <option value={""}>First Option</option>
-              <option value={""}>Second Option</option>
+            <select
+              value={toLanguage}
+              onChange={(e) => setToLanguage(e.target.value)}
+            >
+              {languages.map(({ code, name }) => (
+                <option key={code} value={code}>
+                  {name}
+                </option>
+              ))}
             </select>
             <div className="icons">
-              <FaCopy className="i-to" id="to" />
-              <FaVolumeUp className="i-to" id="to" />
+              <FaCopy
+                className="to-vol"
+                id="to"
+                onClick={() => handleIconClick("copy", toText)}
+              />
+              <FaVolumeUp
+                className="from-copy"
+                id="to"
+                onClick={() => handleIconClick("speak", toText, toLanguage)}
+              />
             </div>
           </li>
         </ul>
