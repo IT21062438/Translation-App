b4f7d7543a860b70630e924edfc5fb3c398870d3
Harendra Samarawickrama <87431873+IT21062438@users.noreply.github.com>
2025-02-04 22:42:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-// import "./App.css";
-// import Translator from "./components/Translator";
-
-// function App() {
-//   return (
-//     <div className="container">
-//       <Translator></Translator>
-//     </div>
-//   );
-// }
-
-// export default App;
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Translator from "./components/Translator";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Translator />} />
-      </Routes>
-    </Router>
+    <div>
+      <Translator></Translator>
+    </div>
   );
 }
 
 export default App;
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Translator from "./components/Translator";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Translator />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
