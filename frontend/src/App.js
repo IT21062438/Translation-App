43d0068ced59945c350c053a9523cce62c79fd68
Harendra Samarawickrama <87431873+IT21062438@users.noreply.github.com>
2025-02-04 22:53:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,17 +10,3 @@ function App() {
 }
 
 export default App;
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Translator from "./components/Translator";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Translator />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
